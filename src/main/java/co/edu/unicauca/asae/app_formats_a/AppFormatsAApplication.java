9f38a74cd77d962da8ed054fe7ea2ee8ece25a20
rscruz <santiagocrzzz1711@gmail.com>
2025-04-23 12:16:48 -0500

    first commit

---
+++ src/main/java/co/edu/unicauca/asae/app_formats_a/AppFormatsAApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae.app_formats_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFormatsAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFormatsAApplication.class, args);
+	}
+
+}
